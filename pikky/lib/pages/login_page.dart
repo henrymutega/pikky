a0a61cab83a4bcf5065898d52aaa12003d287eeb
henrymutega <hmutega@gmail.com>
2024-06-13 09:35:47 +0300

    Login page ui design.

--- pikky/lib/pages/login_page.dart
+++ pikky/lib/pages/login_page.dart
@@ -1,11 +1,31 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 30),
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Colors.pink.shade300,
+          Colors.pink.shade200,
+          Colors.pink.shade100
+        ])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const <Widget>[
+            SizedBox(height: 80),
+            Text(
+              'Login',
+              style: TextStyle(color: Colors.white, fontSize: 40),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
