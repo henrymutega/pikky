c6e19227482caca2a860e8221c4e8fd7d6cac729
henrymutega <hmutega@gmail.com>
2024-06-13 08:20:13 +0300

    Implementing slash screen to the app.

---
+++ pikky/lib/pages/screen_splash.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class ScreenSplash extends StatefulWidget {
+  const ScreenSplash({super.key});
+
+  @override
+  State<ScreenSplash> createState() => _ScreenSplashState();
+}
+
+class _ScreenSplashState extends State<ScreenSplash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Icon(
+          Icons.lock,
+          size: 100,
+        ),
+      ),
+    );
+  }
+}
---
+++ pikky/lib/pages/signup_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
